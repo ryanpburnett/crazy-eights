29a145dc208bc3c9e0e1ac0d528c780ca98ada2b
Ryan <ryanpburnett@yahoo.com>
2022-01-03 11:22:08 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// make deck
+const suit = ["Spades", "Clubs", "Diamonds", "Hearts"];
+const rank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+
+let deck = [];
+
+const makeDeck = () => {
+    for (i = 0; i < suit.length; i++) {
+        for (j = 0; j < rank.length; j++) {
+            deck.push(`${rank[j]} of ${suit[i]}`);
+        }
+    }
+}
+makeDeck();
+
+// shuffle deck, Fischer-Yates shuffle
+let index = deck.length, jindex, temp;
+
+while(index > 0, index--) {
+    jindex = Math.floor(Math.random() * (index + 1));
+    temp = deck[jindex];
+    deck[jindex] = deck[index];
+    deck[index] = temp;
+}
+
+// deal cards
+let p1 = [];
+let p2 = [];
+
+for (i = 0; i < 5; i++) {
+    p1.push(deck.pop());
+    p2.push(deck.pop());
+}
+
